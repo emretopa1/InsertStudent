e519c76cf68281e12ee2dd2bcbc29a5ba1af69db
Emre Topal <74003597+emretopa1@users.noreply.github.com>
2022-06-01 11:04:28 +0300

    Add files via upload

---
+++ ControllerAndView/Controllers/OgrenciController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using InsertToStudent.Models;
+
+namespace InsertToStudent.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        private readonly OkulDbContext _context;
+
+        public OgrenciController(OkulDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ogrenci
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ogrenciler.ToListAsync());
+        }
+
+        // GET: Ogrenci/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ogrenci = await _context.Ogrenciler
+                .FirstOrDefaultAsync(m => m.OgrenciId == id);
+            if (ogrenci == null)
+            {
+                return NotFound();
+            }
+
+            return View(ogrenci);
+        }
+
+        // GET: Ogrenci/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ogrenci/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OgrenciId,OgrenciAd,OgrenciSoyad,OgrenciNo")] Ogrenci ogrenci)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ogrenci);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ogrenci);
+        }
+
+        // GET: Ogrenci/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ogrenci = await _context.Ogrenciler.FindAsync(id);
+            if (ogrenci == null)
+            {
+                return NotFound();
+            }
+            return View(ogrenci);
+        }
+
+        // POST: Ogrenci/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OgrenciId,OgrenciAd,OgrenciSoyad,OgrenciNo")] Ogrenci ogrenci)
+        {
+            if (id != ogrenci.OgrenciId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ogrenci);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OgrenciExists(ogrenci.OgrenciId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ogrenci);
+        }
+
+        // GET: Ogrenci/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ogrenci = await _context.Ogrenciler
+                .FirstOrDefaultAsync(m => m.OgrenciId == id);
+            if (ogrenci == null)
+            {
+                return NotFound();
+            }
+
+            return View(ogrenci);
+        }
+
+        // POST: Ogrenci/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ogrenci = await _context.Ogrenciler.FindAsync(id);
+            _context.Ogrenciler.Remove(ogrenci);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OgrenciExists(int id)
+        {
+            return _context.Ogrenciler.Any(e => e.OgrenciId == id);
+        }
+    }
+}
